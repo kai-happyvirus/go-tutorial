ae4e00cc8a9bd7271f140c6f6485b567b3e85f45
Kai Cho <kai.cho@netlogix.co.nz>
2023-11-15 21:47:28 +1300

    D-1: Constants

--- day-1/hello-world.go
+++ day-1/hello-world.go
@@ -1,6 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "constant"
+
+func constants() {
+	fmt.Println(s)
+
+	const n = 500000000
+
+	const d = 3e20 / n
+	fmt.Println(d)
+
+	fmt.Println(int64(d))
+
+	fmt.Println(math.Sin(n))
+
+}
 
 func variable() {
 
@@ -29,4 +48,5 @@ func main() {
 	fmt.Println("Hello, World!")
 	typeValue()
 	variable()
+	constants()
 }
