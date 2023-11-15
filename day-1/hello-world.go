01452968582749ae80b2498564250f9e070567d3
Kai Cho <kai.cho@netlogix.co.nz>
2023-11-15 21:38:36 +1300

    D-1: Primitive type

--- day-1/hello-world.go
+++ day-1/hello-world.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+func typeValue() {
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println(true && true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
+
 func main() {
 	fmt.Println("Hello, World!")
+	typeValue()
 }
