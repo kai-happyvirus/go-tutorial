02679513d972734bfeac3d8f0e0606266e2fa311
Kai Cho <kai.cho@netlogix.co.nz>
2023-11-16 22:08:06 +1300

    D-2: array

--- day-2/day2.go
+++ day-2/day2.go
@@ -9,6 +9,21 @@ func main() {
 	forLoop()
 	ifElse()
 	switchStatement()
+	arraySequence()
+}
+
+func arraySequence() {
+	var a [5]int
+	fmt.Println("emp:", a)
+
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+	fmt.Println("len:", len(a))
+
+	b := [5]int{1,2,3,4,5}
+	fmt.Println("dcl", b)
+	
 }
 
 func switchStatement() {
