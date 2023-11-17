b27bc42a079e544f69e4cc706a834a86e18be20b
Kai Cho <95102552+kai-happyvirus@users.noreply.github.com>
2023-11-17 22:27:10 +1300

    Day 3 (#3)

---
+++ day-3/greeting/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/kai-happyvirus/go-tutorial/day-3/functions"
+)
+
+func main() {
+	const a = 50
+	fmt.Println(a)
+
+	// declare group of constants
+	const (
+		name    = "Kai"
+		age     = 30
+		country = "Germany"
+	)
+
+	fmt.Println(name, age, country)
+
+	// The value of a constant should be known at compile time.
+	var b = math.Sqrt(4)
+	// const c = math.Sqrt(4) // Not allowed
+	fmt.Println(b)
+	const n = "Sam"
+	var nameOfn = n
+	fmt.Println("type %T value %v", nameOfn, nameOfn)
+
+	var defaultName = "Sam" //allowed
+	type myString string
+	var customName myString = "Sam" //allowed
+	// customName = defaultName //not allowed
+	fmt.Println(customName)
+	fmt.Println(defaultName)
+	numberConstant()
+	price, no := 90, 6
+	totalPrice := functions.CalculateBill(price, no)
+	fmt.Println("Total price is", totalPrice)
+}
+
+func calculateBill(price, no int) {
+	panic("unimplemented")
+}
+
+func numberConstant() {
+	const a = 5
+	var intVar int = a
+	var int32Var int32 = a
+	var float64Var float64 = a
+	var complex64Var complex64 = a
+	fmt.Println("intVar", intVar, "\nint32Var", int32Var, "\nfloat64Var", float64Var, "\ncomplex64Var", complex64Var)
+}
