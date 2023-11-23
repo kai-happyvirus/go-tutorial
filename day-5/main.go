ff7480271f091330038154e2f400a86a42d8e3bb
Kai Cho <kai.cho@netlogix.co.nz>
2023-11-23 22:26:05 +1300

    recursion

--- day-5/main.go
+++ day-5/main.go
@@ -12,6 +12,17 @@ func main() {
 	newInts := intSeq()
 	fmt.Println(newInts())
 
+	fmt.Println(recursion(7))
+
+	var fib func(n int) int
+
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+		return fib(n-1) + fib(n-2)
+	}
+	fmt.Println(fib(7))
 }
 
 func intSeq() func() int {
@@ -20,4 +31,11 @@ func intSeq() func() int {
 		i++
 		return i
 	}
+}
+
+func recursion(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * recursion(n-1)
 }
