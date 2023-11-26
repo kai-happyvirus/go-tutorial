68ea3943e01988f023aca20dc2d43e74e3ecaa8a
Kai Cho <kai.cho@netlogix.co.nz>
2023-11-26 21:22:57 +1300

    methods

--- day-6/main.go
+++ day-6/main.go
@@ -54,13 +54,21 @@ type rect struct {
 	width, height int
 }
 
+// This area method has a receiver type of *rect.
+// Methods can be defined for either pointer or value receiver types.
+// Here’s an example of a value receiver.
 func (r *rect) area() int {
+
 	return r.width * r.height
 }
 
 func (r rect) perim() int {
 	return 2*r.width + 2*r.height
 }
+
+// Go automatically handles conversion between values and pointers for method calls.
+// You may want to use a pointer receiver type to avoid copying on method calls or
+// to allow the method to mutate the receiving struct.
 func methods() {
 	r := rect{width: 10, height: 5}
 
