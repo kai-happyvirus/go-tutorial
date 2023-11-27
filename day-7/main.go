6b587119b43febc1725a6d0afc0f48ffa6c0c1a9
Kai Cho <kai.cho@netlogix.co.nz>
2023-11-27 21:25:40 +1300

    generic

--- day-7/main.go
+++ day-7/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type base struct {
 	num int
@@ -17,6 +19,7 @@ type container struct {
 
 func main() {
 	structEmbadding()
+	generic()
 }
 
 func structEmbadding() {
@@ -33,4 +36,49 @@ func structEmbadding() {
 	}
 	var d describer = co
 	fmt.Println("describer:", d.describe())
+}
+
+func MapKeys[K comparable, V any](m map[K]V) []K {
+	r := make([]K, 0, len(m))
+	for k := range m {
+		r = append(r, k)
+	}
+	return r
+}
+
+type List[T any] struct {
+	head, tail *element[T]
+}
+type element[T any] struct {
+	next *element[T]
+	val  T
+}
+
+func (lst *List[T]) Push(v T) {
+	if lst.tail == nil {
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
+func (lst *List[T]) GetAll() []T {
+	var elems []T
+	for e := lst.head; e != nil; e = e.next {
+		elems = append(elems, e.val)
+	}
+	return elems
+}
+func generic() {
+	var m = map[int]string{1: "2", 2: "4", 4: "8"}
+
+	fmt.Println("keys:", MapKeys(m))
+
+	_ = MapKeys[int, string](m)
+	lst := List[int]{}
+	lst.Push(10)
+	lst.Push(13)
+	lst.Push(23)
+	fmt.Println("list:", lst.GetAll())
 }
